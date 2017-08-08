8a86403017d35d040a97b1c12d33defd299418d2
sareyn <sajas.p15@iiits.in>
2017-08-08 11:57:53 +0530

    Add files via upload

---
+++ mysite/polls/views.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from django.http import Http404
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import loader
+from .models import Question,Choice
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+'''def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return render(request,'polls/index.html',context)
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)'''
+
+'''def detail(request, question_id):
+    #try:
+    #question = Question.objects.get(pk=question_id)
+#except Question.DoesNotExist:
+   # raise Http404("Question does not exists")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request,'polls/detail.html',{'question':question})
+
+ 	#return HttpResponse("<h2>Details for polls id : "+  str(pol_id)+"</h2>")'''
+
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError ,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{'question':question,'error_message':'you didnt select a choice',})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
+
+
+'''def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})'''
